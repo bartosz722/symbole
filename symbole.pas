2ac6a30ef42cde0bfe958d2bf4010e8e6f030864
bartosz722 <bartoszp77@o2.pl>
2022-10-09 21:18:51 +0200

    Strona 4, trochę 5, usunięte niektóre spacje

--- symbole.pas
+++ symbole.pas
@@ -42,18 +42,18 @@ CONST
          (symbol : 'Ar'; nazwa : 'ARGON'  ));
 
 VAR
-    Maxx, Maxy      : word;
-    xcent, ycent    : word;
+    Maxx,Maxy       : word;
+    xcent,ycent     : word;
     Skala           : integer;
     Maxcolor        : word;
-    Height, Width   : word;
+    Height,Width    : word;
     
     animindex       : integer;
     
-    wodax, gorax    : integer;
+    wodax,gorax     : integer;
     
     obrazki         : array [1..14] of pointer;
-    i, j, pozycja   : integer;
+    i,j,pozycja     : integer;
     
     ch              : char;
     symb            : string[2];
@@ -61,19 +61,19 @@ VAR
     punkty          : integer;
     czasreakcji     : word;
     los             : integer;
-    level, licznik  : integer;
+    level,licznik   : integer;
     
     firstletter     : boolean;
-    ok, zderzenie   : boolean;
+    ok,zderzenie    : boolean;
 
 
 procedure Init;
 var
-    driver, mode : integer;
+    driver,mode  : integer;
     errorcode    : integer;
 begin
     driver := Detect;
-    InitGraph(driver, mode, '');
+    InitGraph(driver,mode,'');
     errorcode := GraphResult;
     if errorcode <> grOk then
         begin
@@ -97,17 +97,98 @@ begin
     Width := Textwidth('M');
 end; { Init }
 
+
 procedure welcome;
 begin
     Cleardevice;
-    Outtextxy(20, 20, 'SYMBOLE CHEMICZNE - Gra edukacyjna');
-    Outtextxy(30, 40, 'Instrukcja:');
-    Outtextxy(50, 60, 'Statki o nazwach pierwiastków chemicznych musisz');
-    Outtextxy(40, 70, 'doprowadzić bezpiecznie do portu. Po drodze napotykają');
-    Outtextxy(40, 80, 'one góry lodowe. Aby statek nie rozbił się, należy');
-    Outtextxy(40, 90, 'poprawnie wpisać symbol pierwiastka chemicznego, którego');
-    Outtextxy(40, 100, 'nazwę nosi statek.');
-    Outtextxy(30, 120, 'Aby zacząć grę - wpisz 1 lub 2, gdy chcesz skończyć wciścnij ESC.');
-    Outtextxy(50, 140, '1 - gra na podstawowym poziomie (18 pierwiastków)');
-    Outtextxy(50, 150, '2 - poziom bardziej zaawansowany');
+    Outtextxy(20,20,'SYMBOLE CHEMICZNE - Gra edukacyjna');
+    Outtextxy(30,40,'Instrukcja:');
+    Outtextxy(50,60,'Statki o nazwach pierwiastków chemicznych musisz');
+    Outtextxy(40,70,'doprowadzić bezpiecznie do portu. Po drodze napotykają');
+    Outtextxy(40,80,'one góry lodowe. Aby statek nie rozbił się, należy');
+    Outtextxy(40,90,'poprawnie wpisać symbol pierwiastka chemicznego, którego');
+    Outtextxy(40,100,'nazwę nosi statek.');
+    Outtextxy(30,120,'Aby zacząć grę - wpisz 1 lub 2, gdy chcesz skończyć wciścnij ESC.');
+    Outtextxy(50,140,'1 - gra na podstawowym poziomie (18 pierwiastków)');
+    Outtextxy(50,150,'2 - poziom bardziej zaawansowany');
 end; { welcome }
+
+
+procedure brr(j:integer);
+begin
+    sound(j);
+    delay(15);
+    nosound;
+end; { brr }
+
+
+procedure learn;
+var
+    size : word;
+    i    : integer;
+
+procedure rysstatek(x,y:word);
+begin
+    setcolor(Maxcolor);
+    moveto(x,y);
+    linerel(160,0);
+    linerel(-12,0);
+    linerel(2,-2);
+    linerel(10,-18);
+    linerel(-30,0);
+    linerel(-5,5);
+    linerel(-90,0);
+    linerel(-1,-15);    { nadbud }
+    linerel(3,0);
+    linerel(-40,0);
+    linerel(3,0);
+    linerel(0,15);
+    linerel(34,0);
+    linerel(-45,0);     { ruf }
+    linerel(1,9);
+    linerel(1,3);
+    linerel(2,2);
+    linerel(3,1);
+    linerel(4,0);
+    
+    line(x+70,y-16,x+70,y-40);  { dźwigi }
+    line(x+70,y-25,x+60,y-37);
+    line(x+110,y-16,x+110,y-40);
+    line(x+110,y-25,x+100,y-37);
+    
+    line(x+40,y-16,x+67,y-16);  { ładownie }
+    line(x+40,y-17,x+67,y-17);
+    line(x+75,y-16,x+107,y-16);
+    line(x+75,y-17,x+107,y-17);
+    
+    circle(x+145,y-15,1);
+    circle(x+16,y-25,2);
+    circle(x+24,y-25,2);
+end; { rysstatek }
+
+begin
+
+{------------------------------------------------------------------------------}
+{                   Zapamiętanie obrazków do animacji.                         }
+{------------------------------------------------------------------------------}
+
+{ ******** Statek ******** }
+
+size = imagesize(4,140,190,180);
+for i:=1 to 4 do getmem(obrazki[i],size);
+
+rysstatek(30,180);
+
+putpixel(13,179,Maxcolor);  { pierwszy image }
+putpixel(16,179,Maxcolor);
+putpixel(18,179,Maxcolor);
+putpixel(19,179,Maxcolor);
+
+for i:=0 to 14 do
+    begin;
+        putpixel(34+i*10,180,0);
+        putpixel(34+i*10,179,Maxcolor);
+    end;
+    
+getimage(4,140,190,180,obrazki[1]^);
+
